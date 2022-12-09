c8c250dd1739e1000533255deb6ecf2a8979189f
opluss <opluss@qq.com>
2022-12-09 09:57:41 +0800

    feat: Add env init script (rough)

---
+++ script/init_rt_env.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+source "$(cd "$(dirname "${0}")"; pwd)/utils/common.sh" > /dev/null
+
+if [ "${OS_NAME}" = "ubuntu" ]
+then
+  CMD="libgl-dev libglu-dev 'libxcb*-dev' libx11-xcb-dev libxkbcommon-x11-dev libpcre2-dev libz-dev libfreetype6-dev libpng-dev libjpeg-dev libsqlite3-dev libharfbuzz-dev libb2-dev libdouble-conversion-dev"
+  if [ "${OS_VER}" = "22.04" ]
+  then
+    CMD="${CMD} libmd4c-dev"
+  elif [ "${OS_VER}" = "20.04" ]
+  then
+    CMD="${CMD} libmd4c-dev"
+  elif [ "${OS_VER}" = "18.04" ]
+  then
+    CMD="${CMD}"
+  else
+    echo "Unsupport OS Version: ${OS_VER}"
+    exit 1
+  fi
+  echo "Will Install: ${CMD}"
+  bash -c "sudo apt install ${CMD}"
+else
+  echo "Unsupport OS: ${OS_NAME}"
+  exit 1
+fi
+
